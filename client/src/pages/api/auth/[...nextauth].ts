ce5aa07321031115c33366671476af879d9f053f
alexrobaina <alexrobainaph@gmail.com>
2022-06-12 18:57:59 -0300

    prisma config

--- client/src/pages/api/auth/[...nextauth].ts
+++ client/src/pages/api/auth/[...nextauth].ts
@@ -2,10 +2,14 @@ import NextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import FacebookProvider from 'next-auth/providers/facebook';
 import GoogleProvider from 'next-auth/providers/google';
-import prisma from '../../../prisma/prisma';
+// import prisma from '../../../prisma/prisma';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const options = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -28,13 +32,24 @@ const options = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
   ],
+  callbacks: {
+    async session({ session, user }: any) {
+      return {
+        ...session,
+        user,
+      };
+    },
+    // async signIn({ user }: any) {
+    //   // Check for a development environment
+    //   if (process.env.NODE_ENV === 'development') return true;
+    // },
+  },
   // adapter: PrismaAdapter(prisma),
   // callbacks: {
   //   session: async ({ session }: any) => {
   //     return Promise.resolve(session);
   //   },
   // },
-  // adapter: PrismaAdapter(prisma),
 };
 
 export default (req: any, res: any) => NextAuth(req, res, options);
